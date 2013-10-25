010249eb94e6272194c7df7996e4fa79cd1732bc
HuyNQ <huy@huynq.net>
2013-10-25 12:32:55 +0700

    Minify CSS and Javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,151 @@
 (function ($) {
     "use strict";
+
+    function get_feature_image() {
+        var articles_links = $(".post-title a"),
+            featured_parts = $(".featured"),
+            list_links = [];
+
+        /* -- Feature Images and Video per Post -- */
+        if (articles_links.length > 0) {
+            articles_links.each(function (index) {
+                //Process script                 
+                list_links.push(articles_links[index].href);
+                //localStorage.setItem(articles_links[index].href, index);
+            });
+        }
+
+        if (list_links.length > 0) {
+            list_links.forEach(function (element, index, array) {
+                $.get(list_links[index], function (data) {
+                    var html = $(data),
+                        article = html.find("article"),
+                        img = article.find("img:first"),
+                        video = article.find("iframe:first"),
+                        featured = featured_parts[index];
+
+                    if (img.length > 0 && video.length > 0) {
+                        $(featured).html(img[0]);
+                    } else {
+                        if (img.length > 0) {
+                            $(featured).html(img[0]);
+                        } else if (video.length > 0) {
+                            $(featured).html(video[0].outerHTML);
+                        }
+                    }
+                    $(".post").fitVids();
+                });
+            });
+        }
+    }
+
+    function add_weather_emo() {
+        // rain, cloudy, sunny, cold
+        var metaNodes = $(".post-meta"),
+            weatherNodes = $(".post-weather"),
+            weather_hash = {
+                "rain": "wi-rain",
+                "cloudy": "wi-cloudy",
+                "sunny": "wi-day-sunny",
+                "cold": "wi-snow"
+            },
+            weather_arr = [],
+            // happy, sad, okay
+            emoNodes = $(".post-emoticon"),
+            emo_arr = [];
+            /*emo_hash = {
+                "happy": "vui",
+                "sad": "chan",
+                "okay": "bt"
+            },*/
+
+        /* -- Add weather -- */
+        metaNodes.each(function (index) {
+            var matched_weather = $(this).text().match("rain|cloudy|sunny|cold");
+            weather_arr.push((matched_weather === null ? "sunny" : matched_weather).toString());
+        });
+
+        weatherNodes.each(function (index) {
+            if ($(window).width() >= 500) {
+                $(this).html("<i class=\"" + weather_hash[weather_arr[index]] + "\">");
+            }
+        });
+
+        /* -- Add mood -- */
+        metaNodes.each(function (index) {
+            var matched_mood = $(this).text().match("happy|sad|okay");
+            emo_arr.push((matched_mood === null ? "okay" : matched_mood).toString());
+        });
+
+        emoNodes.each(function (index) {
+            if ($(window).width() >= 500) {
+                $(this).addClass(emo_arr[index]);
+            }
+        });
+    }
+
+    //Function check menu opened
+    function move_scroll_icon() {
+        if (menu_open()) {
+            $(".scrollup").removeClass("scroll-right");
+            $(".scrollup").addClass("scroll-left");
+        } else {
+            $(".scrollup").removeClass("scroll-left");
+            $(".scrollup").addClass("scroll-right");
+        }
+    }
+
+    //Function generate background
+
+    function getbg() {
+        var d = new Date(),
+            today = parseInt(d.getDate(), 10),
+            bg = "1.png";
+
+        if (today <= 5) {
+            bg = "6.png";
+        } else if (today <= 10) {
+            bg = "5.png";
+        } else if (today <= 15) {
+            bg = "4.png";
+        } else if (today <= 20) {
+            bg = "3.png";
+        } else if (today <= 25) {
+            bg = "2.png";
+        } else {
+            bg = "1.png";
+        }
+        return bg;
+    }
+
+    function menu_open() {
+        return ($("#menu").css("right") === "0px");
+    }
+
+    function detect_browser() {
+        var browser = {
+            chrome: false,
+            mozilla: false,
+            opera: false,
+            msie: false,
+            safari: false
+        },
+            sBrowser, sUsrAg = navigator.userAgent;
+
+        if (sUsrAg.indexOf("Chrome") > -1) {
+            browser.chrome = true;
+        } else if (sUsrAg.indexOf("Safari") > -1) {
+            browser.safari = true;
+        } else if (sUsrAg.indexOf("Opera") > -1) {
+            browser.opera = true;
+        } else if (sUsrAg.indexOf("Firefox") > -1) {
+            browser.mozilla = true;
+        } else if (sUsrAg.indexOf("MSIE") > -1 || sUsrAg.indexOf("Trident") > -1) {
+            browser.msie = true;
+        }
+        return browser;
+    }
+
     $(document).ready(function () {
 
         /* -- Detect IE -- */
@@ -101,7 +247,7 @@
             }
         });
 
-        $(".scrollup,.no-more").click(function () {
+        $(".scrollup").click(function () {
             $("html, body").animate({
                 scrollTop: 0
             }, 1000);
@@ -112,149 +258,4 @@
         add_weather_emo();
     });
 
-    function get_feature_image() {
-        var articles_links = $(".post-title a"),
-            featured_parts = $(".featured"),
-            list_links = [];
-
-        /* -- Feature Images and Video per Post -- */
-        if (articles_links.length > 0) {
-            articles_links.each(function (index) {
-                //Process script                 
-                list_links.push(articles_links[index].href);
-                //localStorage.setItem(articles_links[index].href, index);
-            });
-        }
-
-        if (list_links.length > 0) {
-            list_links.forEach(function (element, index, array) {
-                $.get(list_links[index], function (data) {
-                    var html = $(data),
-                        article = html.find("article"),
-                        img = article.find("img:first"),
-                        video = article.find("iframe:first"),
-                        featured = featured_parts[index];
-
-                    if (img.length > 0 && video.length > 0) {
-                        $(featured).html(img[0]);
-                    } else {
-                        if (img.length > 0) {
-                            $(featured).html(img[0]);
-                        } else if (video.length > 0) {
-                            $(featured).html(video[0].outerHTML);
-                        }
-                    }
-                    $(".post").fitVids();
-                });
-            });
-        }
-    }
-
-    function add_weather_emo() {
-        // rain, cloudy, sunny, cold
-        var metaNodes = $(".post-meta"),
-            weatherNodes = $(".post-weather"),
-            weather_hash = {
-                "rain": "wi-rain",
-                "cloudy": "wi-cloudy",
-                "sunny": "wi-day-sunny",
-                "cold": "wi-snow"
-            },
-            weather_arr = [],
-            // happy, sad, okay
-            emoNodes = $(".post-emoticon"),
-            emo_hash = {
-                "happy": "vui",
-                "sad": "chan",
-                "okay": "bt"
-            },
-            emo_arr = [];
-
-        /* -- Add weather -- */
-        metaNodes.each(function (index) {
-            var matched_weather = $(this).text().match("rain|cloudy|sunny|cold");
-            weather_arr.push((matched_weather === null ? "sunny" : matched_weather).toString());
-        });
-
-        weatherNodes.each(function (index) {
-            if ($(window).width() >= 500) {
-                $(this).html("<i class=\"" + weather_hash[weather_arr[index]] + "\">");
-            }
-        });
-
-        /* -- Add mood -- */
-        metaNodes.each(function (index) {
-            var matched_mood = $(this).text().match("happy|sad|okay");
-            emo_arr.push((matched_mood === null ? "okay" : matched_mood).toString());
-        });
-
-        emoNodes.each(function (index) {
-            if ($(window).width() >= 500) {
-                $(this).addClass(emo_arr[index]);
-            }
-        });
-    }
-
-    //Function check menu opened
-
-    function move_scroll_icon() {
-        if (menu_open()) {
-            $(".scrollup").removeClass("scroll-right");
-            $(".scrollup").addClass("scroll-left");
-        } else {
-            $(".scrollup").removeClass("scroll-left");
-            $(".scrollup").addClass("scroll-right");
-        }
-    }
-
-    //Function generate background
-
-    function getbg() {
-        var d = new Date(),
-            today = parseInt(d.getDate(), 10),
-            bg = "1.png";
-
-        if (today <= 5) {
-            bg = "6.png";
-        } else if (today <= 10) {
-            bg = "5.png";
-        } else if (today <= 15) {
-            bg = "4.png";
-        } else if (today <= 20) {
-            bg = "3.png";
-        } else if (today <= 25) {
-            bg = "2.png";
-        } else {
-            bg = "1.png";
-        }
-        return bg;
-    }
-
-    function menu_open() {
-        return ($("#menu").css("right") === "0px");
-    }
-
-    function detect_browser() {
-        var browser = {
-            chrome: false,
-            mozilla: false,
-            opera: false,
-            msie: false,
-            safari: false
-        },
-            sBrowser, sUsrAg = navigator.userAgent;
-
-        if (sUsrAg.indexOf("Chrome") > -1) {
-            browser.chrome = true;
-        } else if (sUsrAg.indexOf("Safari") > -1) {
-            browser.safari = true;
-        } else if (sUsrAg.indexOf("Opera") > -1) {
-            browser.opera = true;
-        } else if (sUsrAg.indexOf("Firefox") > -1) {
-            browser.mozilla = true;
-        } else if (sUsrAg.indexOf("MSIE") > -1 || sUsrAg.indexOf("Trident") > -1) {
-            browser.msie = true;
-        }
-        return browser;
-    }
 }(jQuery));
