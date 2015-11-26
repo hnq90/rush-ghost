5d868fb1c157ccad06c3f668753ac752be2c901a
HuyNQ <huy@huynq.net>
2015-11-26 06:47:34 +0000

    Fix link in tumblr widget

--- assets/js/fetch_tumblr.sh
+++ assets/js/fetch_tumblr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O tumblr_api.js http://sose.xyz/api/read/json?number=8&type=photo
+wget -O /var/www/ghost/content/themes/minimalism-ghost/assets/js/tumblr_api.js http://sose.xyz/api/read/json?number=8&type=photo
