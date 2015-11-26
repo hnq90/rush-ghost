0dd793c34e1330aa496ce579ed3f005f3c27f700
HuyNQ <huy@huynq.net>
2015-11-26 14:26:15 +0700

    Refactor Tumblr widget

--- assets/js/fetch_tumblr.sh
+++ assets/js/fetch_tumblr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O /var/www/ghost/content/themes/minimalism-ghost/assets/js/tumblr_api.js http://sose.xyz/api/read/json?number=8&type=photo
+wget -O /var/www/ghost/content/themes/minimalism-ghost/assets/js/tumblr_api.js http://sose.xyz/api/read/json?num=7&type=photo
---
+++ assets/js/tumblr_widget.js
@@ -0,0 +1,12 @@
+if (tumblr_api_read) {
+  var tumblr_widget = document.getElementById('htumblr-widget');
+  var html = '';
+
+  if (tumblr_api_read.posts.length > 0) {
+    Array.prototype.forEach.call(tumblr_api_read.posts, function(post, i){
+      html += '<a href="' + tumblr_api_read.posts[i]['url'] + '" target="_blank"><img border="0" style="margin:0" src="' + tumblr_api_read.posts[i]['photo-url-75'].replace('http:', '') + '"/></a>\n';
+    });
+    tumblr_widget.setAttribute('style', 'margin: 0 auto;max-width: 650px;');
+    tumblr_widget.innerHTML = html;
+  }
+}
